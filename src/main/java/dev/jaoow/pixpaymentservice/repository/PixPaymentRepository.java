729f7efb4ac1e654b789a7b2060596b4cacbbf79
Jaoow <xfx.joao@gmail.com>
2024-06-28 10:37:05 -0300

    refactor: Add @Repository annotation to PixPaymentRepository

--- src/main/java/dev/jaoow/pixpaymentservice/repository/PixPaymentRepository.java
+++ src/main/java/dev/jaoow/pixpaymentservice/repository/PixPaymentRepository.java
@@ -2,7 +2,9 @@ package dev.jaoow.pixpaymentservice.repository;
 
 import dev.jaoow.pixpaymentservice.entity.PixPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PixPaymentRepository extends JpaRepository<PixPayment, Long> {
     PixPayment findByPaymentId(Long paymentId);
 }
